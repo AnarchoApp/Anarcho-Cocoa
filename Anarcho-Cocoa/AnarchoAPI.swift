12f5fcdee6b9c279507d5f95573684177df51621
Dima <dsay@ciklum.com>
2015-05-20 10:33:17 +0300

    update api

--- Anarcho-Cocoa/AnarchoAPI.swift
+++ Anarcho-Cocoa/AnarchoAPI.swift
@@ -10,6 +10,14 @@ public struct Application {
     let name : NSString
 }
 
+public struct Build {
+    let id : NSInteger
+    let created_on : NSTimeInterval
+    let release_notes : NSString
+    let version_code : NSString
+    let version_name : NSString
+}
+
 public typealias completion = () -> Void
 
 public class AnarchoAPI{
@@ -19,9 +27,11 @@ public class AnarchoAPI{
             .validate(statusCode: 200..<200)
             .validate(contentType: ["application/json"])
             .responseString(encoding: NSUTF8StringEncoding) { (_, _, string, _) -> Void in
-                var mapper = JsonSwiftson(json: string!)
-                let token : NSString = mapper["authToken"].map()!
-                Alamofire.Manager.sharedInstance.session.configuration.HTTPAdditionalHeaders = ["x-auth-token" :token]
+                if let json = string{
+                    var mapper = JsonSwiftson(json: json)
+                    let token : NSString = mapper["authToken"].map()!
+                    Alamofire.Manager.sharedInstance.session.configuration.HTTPAdditionalHeaders = ["x-auth-token" :token]
+                }
                 if let block = completionBlock {
                     block()
                 }
@@ -34,21 +44,89 @@ public class AnarchoAPI{
             .validate(statusCode: 200..<200)
             .validate(contentType: ["application/json"])
             .responseString(encoding: NSUTF8StringEncoding) { (_, _, string, _) -> Void in
-                var mapper = JsonSwiftson(json: string!)
-                let applications : [Application] = mapper["list"].mapArrayOfObjects { j in
-                    Application(
-                        app_key: j["app_key"].map()!,
-                        app_type: j["app_type"].map()!,
-                        icon_url: j["icon_url"].map()!,
-                        package: j["package"].map()!,
-                        name: j["name"].map()!)
-                    } ?? []
-                completion(applications: applications)
+                if let json = string{
+                    var mapper = JsonSwiftson(json: json)
+                    let applications : [Application] = mapper["list"].mapArrayOfObjects { j in
+                        Application(
+                            app_key: j["app_key"].map()!,
+                            app_type: j["app_type"].map()!,
+                            icon_url: j["icon_url"].map()!,
+                            package: j["package"].map()!,
+                            name: j["name"].map()!)
+                        } ?? []
+                    completion(applications: applications)
+                }else{
+                    completion(applications: [])
+                }
         }
         return self
     }
     
-//    public func then(fungg: () -> Self) -> Self {
-//        return self
-//    }
+    public func getBuilds(appKey: NSString, completion: (builds: [Build]) -> Void) -> Self{
+        Alamofire.request(.GET, ("http://anarcho.pp.ciklum.com/api/apps/\(appKey)/builds"))
+            .validate(statusCode: 200..<200)
+            .validate(contentType: ["application/json"])
+            .responseString(encoding: NSUTF8StringEncoding) { (_, _, string, _) -> Void in
+                if let json = string{
+                    var mapper = JsonSwiftson(json: json)
+                    let builds : [Build] = mapper["list"].mapArrayOfObjects { j in
+                        Build(
+                            id: j["id"].map()!,
+                            created_on: j["created_on"].map()!,
+                            release_notes: j["release_notes"].map()!,
+                            version_code: j["version_code"].map()!,
+                            version_name: j["version_name"].map()!)
+                        } ?? []
+                    completion(builds: builds)
+                }else{
+                    completion(builds: [])
+                }
+        }
+        return self
+    }
+}
+
+class ImageLoader {
+    
+    var cache = NSCache()
+    
+    class var sharedLoader : ImageLoader {
+        struct Static {
+            static let instance : ImageLoader = ImageLoader()
+        }
+        return Static.instance
+    }
+    
+    func imageForUrl(urlString: String, completionHandler:(image: UIImage?, url: String) -> ()) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), {()in
+            var data: NSData? = self.cache.objectForKey(urlString) as? NSData
+            
+            if let goodData = data {
+                let image = UIImage(data: goodData)
+                dispatch_async(dispatch_get_main_queue(), {() in
+                    completionHandler(image: image, url: urlString)
+                })
+                return
+            }
+            
+            var downloadTask: NSURLSessionDataTask = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: urlString)!, completionHandler: {(data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
+                if (error != nil) {
+                    completionHandler(image: nil, url: urlString)
+                    return
+                }
+                
+                if data != nil {
+                    let image = UIImage(data: data)
+                    self.cache.setObject(data, forKey: urlString)
+                    dispatch_async(dispatch_get_main_queue(), {() in
+                        completionHandler(image: image, url: urlString)
+                    })
+                    return
+                }
+                
+            })
+            downloadTask.resume()
+        })
+        
+    }
 }
--- Anarcho-Cocoa/BuildsViewController.swift
+++ Anarcho-Cocoa/BuildsViewController.swift
@@ -1,22 +1,60 @@
 import UIKit
-
+import CoreData
 class BuildsViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
-
+    var applications : [Application] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        AnarchoAPI().autorizaetion("", password: "") { () -> Void in
-            AnarchoAPI().getApps { (applications) -> Void in
-                println(applications)
+        title = "Applications"
+        AnarchoAPI().autorizaetion("dsay@ciklum.com", password: "8hYDrdiv") { () -> Void in
+            AnarchoAPI().getApps { (app) -> Void in
+                self.applications = app
+                self.tableView.reloadData()
             }
+            AnarchoAPI().getBuilds("0ec78bb8-c977-11e4-afd3-62dd4457b65e", completion: { (builds) -> Void in
+                println(builds)
+            })
         }
+    }
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60
+    }
+   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return self.applications.count
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10;
+        return 1
     }
-
+    
+//   override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        let app = self.applications[section]
+//        return app.app_type as String
+//    }
+//    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        var cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        
+        var button = HamburgerButton(frame: CGRectMake(0, 0, 54, 54))
+        button.addTarget(self, action: "toggle:", forControlEvents:.TouchUpInside)
+        cell.accessoryView = button
+        
+        let app = self.applications[indexPath.section]
+        cell.textLabel?.text = app.name as String
+        cell.detailTextLabel?.text = app.package as String
+        
+        cell.imageView?.image = UIImage(named: "App")
+        ImageLoader.sharedLoader.imageForUrl(app.icon_url as String, completionHandler: { (image, url) -> () in
+            if let appIcon = image{
+                cell.imageView?.image = appIcon
+            }
+        })
+        return cell
+    }
+    
+    func toggle(sender: HamburgerButton!) {
+        if let button : HamburgerButton = sender {
+            button.showsMenu = !button.showsMenu
+        }
     }
 }
